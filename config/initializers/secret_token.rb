94bd82371175a26f42895f1b78ffca8062f345bf
Devin <devin@devin-LT21.(none)>
2011-10-21 20:16:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloobelly::Application.config.secret_token = 'acd6e586ee387bff0a8d70976532eafa22f2c18c2ab3a3a9f45a19815cc7530824c3451b4ba4d9d5d93081debfc1cfcf493bd9b50c04b3b41ca7e916a688d515'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bloobelly::Application.config.session_store :cookie_store, key: '_bloobelly_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bloobelly::Application.config.session_store :active_record_store
