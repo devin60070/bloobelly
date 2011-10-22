395c5f3a6a523b42fb66b465f09fcb50e9b94701
Devin <devin@devin-LT21.(none)>
2011-10-22 17:04:15 +0100

    daflj

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 
-conditions = { :email => 'devin.lundberg@gmail.com', 
-               :password => '123',
-               :password_confirmation => '123' }
-
-pp = User.where({ :email => 'devin.lundberg@gmail.com'}).first || User.create(conditions)
+# conditions = { :email => 'devin.lundberg@gmail.com', 
+#                :password => '123',
+#                :password_confirmation => '123' }
+# 
+# pp = User.where({ :email => 'devin.lundberg@gmail.com'}).first || User.create(conditions)
 
 Food.create(
     :name => 'rice',
@@ -24,26 +24,26 @@ Food.create(
     :water_usage=> 35,
     :measure => 'Dozen'
 )
-
-ShoppingTrip.create(
-  :total=> 1235,
-  :user_id=>pp.id
-  )
-
-FoodItem.create(
-    :food_id => 1,
-    :number =>2,
-    :shopping_trip_id =>1
-
-)    
-
-FoodItem.create(
-    :food_id => 2,
-    :number =>1,
-    :shopping_trip_id =>1
-
-)    
-
-
-
-  
+# 
+# ShoppingTrip.create(
+#   :total=> 1235,
+#   :user_id=>pp.id
+#   )
+# 
+# FoodItem.create(
+#     :food_id => 1,
+#     :number =>2,
+#     :shopping_trip_id =>1
+# 
+# )    
+# 
+# FoodItem.create(
+#     :food_id => 2,
+#     :number =>1,
+#     :shopping_trip_id =>1
+# 
+# )    
+# 
+# 
+# 
+#   
