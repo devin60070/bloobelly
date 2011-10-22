d2574fd9640f41bac00fa2232e08e9449a492cce
Devin <devin@devin-LT21.(none)>
2011-10-22 14:32:04 +0100

    added belongs to

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,3 +1,4 @@
 class FoodItem < ActiveRecord::Base
   has_one :food
+  belongs_to :shopping_trip
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+
+conditions = { :email => 'devin60070@gmail.com', 
+               :password => '123',
+               :password_confirmation => '123' }
+
+pp = User.where({conditions}).first || User.create(conditions)
+
 Food.create(
     :name => 'rice',
     :water_usage=> '600',
@@ -17,3 +24,26 @@ Food.create(
     :water_usage=> '35',
     :measure => 'Dozen'
 )
+
+Shopping_trip.create(
+  :total=> '1235',
+  :user_id=>1
+  )
+
+Food_item.create(
+    :food_id => 1,
+    :number =>2,
+    :shopping_trip_id =>1
+
+)    
+
+Food_item.create(
+    :food_id => 2,
+    :number =>1,
+    :shopping_trip_id =>1
+
+)    
+
+
+
+  
